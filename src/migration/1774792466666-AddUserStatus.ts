b61f823924f29a3f5a3f252057057e82249b807e
Jaymax-cyber <jossycode0@gmail.com>
2026-03-29 14:55:22 +0100

    updt

---
+++ src/migration/1774792466666-AddUserStatus.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUserStatus1774792466666 implements MigrationInterface {
+    name = 'AddUserStatus1774792466666'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "unimart"."users_status_enum" AS ENUM('active', 'banned')`);
+        await queryRunner.query(`ALTER TABLE "unimart"."users" ADD "status" "unimart"."users_status_enum" NOT NULL DEFAULT 'active'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "unimart"."users" DROP COLUMN "status"`);
+        await queryRunner.query(`DROP TYPE "unimart"."users_status_enum"`);
+    }
+
+}
